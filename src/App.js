45fd9144f07fdeae1c06f540ffe5613f6654583e
moskalyk <morgan.moskalyk@protonmal.ch>
2021-11-01 17:36:49 -0400

    added simple app with some style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <h1 className='title' style={{color: 'white'}}>
+      R e e d
+    </h1>
+
+      <Grid container spacing={6}>
+        <Grid item xs={4}>
+            <div class="card">
+              <img width={"90%"} height={'90%'} src={'https://www.trustedtarot.com/images/cards/CP.png'}/>
+            </div>
+        </Grid>
+
+        <Grid item xs={4}>
+            <div class="card">
+              <img width={"90%"} height={'90%'} src={'https://www.trustedtarot.com/images/cards/W5.png'}/>
+
+            </div>
+        </Grid>
+
+        <Grid item xs={4}>
+            <div class="card">
+              <img width={"90%"} height={'90%'} src={'https://www.trustedtarot.com/images/cards/04.png'}/>
+            </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
